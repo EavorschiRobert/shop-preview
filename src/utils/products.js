acb8e92a8ab60186de79d19794eafd054ce5f3e5
Eavorschi Robert Daniel <eavorschirobertdaniel@gmail.com>
2024-05-15 09:13:31 +0200

    remove pics

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,29 +1,3 @@
-export const productsCategory = [
-    {
-        title: 'Care Products',
-        category: 'care',
-        description: 'Take care of your mind, body, and soul with our natural products',
-        imageUrl: 'assets/products-category/pexels-cup-of-couple-8015785.jpg'
-    },
-    {
-        title: 'Drinks',
-        category: 'drink',
-        description: 'You Deserve a Refreshment Like No Other, Try our drinks!',
-        imageUrl: '/assets/products-category/pexels-pixabay-461415.jpg'
-    },
-    {
-        title: 'Cleaning',
-        category: 'clean',
-        description: 'All your cleaning needs in one place!',
-        imageUrl: 'assets/products-category/pexels-ron-lach-10557898.jpg'
-    },
-    {
-        title: 'Food',
-        category: 'food',
-        description: 'Your Favorite Neighborhood Store',
-        imageUrl: 'assets/products-category/pexels-ella-olsson-572949-1640777.jpg'
-    }
-]
 export const filterCategory = [
     'care', 'drink','clean','food', 'all'
 ]
